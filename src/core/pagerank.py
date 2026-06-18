9125e7ef8d16166198f236abc3b79d5d2016018b
Cristian Cáceres <134634536+Cristian0Caceres@users.noreply.github.com>
2026-06-17 20:59:22 -0400

    Update pagerank.py

--- src/core/pagerank.py
+++ src/core/pagerank.py
@@ -4,7 +4,7 @@ def Pagerank(_Matrix, _I = 10, _P = 0.85):
     _len = len(_Matrix)
     PR = np.ones(_len) / _len
 
-    for iteracion in range(10):
+    for iteracion in range(_I):
 
         PR = _P * _Matrix @ PR + (1-_P)/_len
 
@@ -12,3 +12,4 @@ def Pagerank(_Matrix, _I = 10, _P = 0.85):
             f"Iteración {iteracion+1}:",
             PR
         )
+    return PR
